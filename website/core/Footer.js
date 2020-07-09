d72bb076ef051e12d346f06dd871137110b2fd18
Vincent Emonet <vincent.emonet@gmail.com>
2020-07-09 16:47:01 +0200

    add ga tracking in footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -135,6 +135,15 @@ class Footer extends React.Component {
           </div>
           {this.props.config.copyright}
         </section>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-172146359-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-172146359-1');
+        </script>
         <Gitter room="um-dsri/community" title="Chat with the community" />
       </footer>
     );
