a3ff8a05df6b623d46346f33afea2649ef765c8f
Vincent Emonet <vincent.emonet@gmail.com>
2020-12-04 02:15:24 +0100

    add local search on docusaurus v2

--- v2-website/docusaurus.config.js
+++ v2-website/docusaurus.config.js
@@ -78,7 +78,20 @@ module.exports={
       }
     ]
   ],
-  "plugins": [],
+  "plugins": [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en"],
+        // ```
+      },
+    ],
+  ],
   "themeConfig": {
     "navbar": {
       "title": "Data Science Research Infrastructure",
