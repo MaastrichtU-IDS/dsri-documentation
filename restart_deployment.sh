4859c8d88c0faf6c622132b1ba0b8a1d84688359
Vincent Emonet <vincent.emonet@gmail.com>
2022-01-17 14:02:54 +0100

    update gitignore, workflow and restart script

---
+++ restart_deployment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Use cache:
+ssh ids1 'cd /data/deploy-ids-tests/dsri-documentation ; git pull ; docker-compose down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --build -d'
+
+## Build without cache:
+# ssh ids1 'cd /data/deploy-ids-tests/dsri-documentation ; git pull ; docker-compose down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d'
