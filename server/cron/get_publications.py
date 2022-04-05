8ba9dc146622d40c974537e4203b3b01a8f3db34
Vincent Emonet <vincent.emonet@gmail.com>
2022-04-05 19:05:08 +0200

    add get_pub script in cron (not working reliably) and add citations in aknowledgments

---
+++ server/cron/get_publications.py
@@ -0,0 +1,27 @@
+import json
+from scholarly import scholarly
+
+# pip install scholarly
+
+# will paginate to the next page by default
+pubs = scholarly.search_pubs("This research was made possible, in part, using the Data Science Research Infrastructure (DSRI) hosted at Maastricht University")
+
+valid_pubs = []
+limit = 20
+
+for i, pub in enumerate(pubs):
+    if i >= limit:
+        break
+    if 'data science research infrastructure' in pub['bib']['abstract'].lower():
+        if 'dsri' in pub['bib']['abstract'].lower():
+            if 'maastricht university' in pub['bib']['abstract'].lower():
+                valid_pubs.append({
+                    'title': pub['bib']['title'],
+                    'authors': ', '.join(pub['bib']['author']),
+                    'pub_year': pub['bib']['pub_year'],
+                    'venue': pub['bib']['venue'],
+                    'abstract': pub['bib']['abstract'],
+                    'url': pub['pub_url'],
+                })
+
+print(json.dumps(valid_pubs, indent=2))
--- website/src/pages/acknowledgement.md
+++ website/src/pages/acknowledgement.md
@@ -13,3 +13,7 @@ The DSRI is supported by funding from the Maastricht University I-Board.
 If you are planning to present or publish your work which was made possible by using the DSRI, we encourage you to acknowledge the use of DSRI. For this purpose we propose to add the following sentence to your publication: 
 
 **"This research was made possible, in part, using the Data Science Research Infrastructure (DSRI) hosted at Maastricht University."**
+
+## They used the DSRI for their publications
+
+* **PSnpBind: a database of mutated binding site protein-ligand complexes constructed using a multithreaded virtual screening workflow**, *A Ammar, R Cavill, C Evelo, E Willighagen*, [Journal of Cheminformatics](https://link.springer.com/article/10.1186/s13321-021-00573-5), 2022
