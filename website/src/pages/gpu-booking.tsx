bf7f29d8f18fa564f7ebedb7dbe6a25a18080bf4
Adelina Tamas <adellina.cucu@gmail.com>
2026-04-14 13:15:25 +0200

    Make sure the text is black [RCS-912]

--- website/src/pages/gpu-booking.tsx
+++ website/src/pages/gpu-booking.tsx
@@ -433,6 +433,7 @@ function GpuBooking() {
                 style={{
                   backgroundColor: "#81c784", 
                   padding: '12px 25px', 
+                  color: '#000000', 
                   width: 'fit-content',      /* Makes box only as wide as the text */
                   margin: '0 auto',          /* Centers the box */
                   whiteSpace: 'nowrap',      /* Forces the text to stay in one row */
@@ -448,6 +449,7 @@ function GpuBooking() {
     style={{
       backgroundColor: "#e57373", 
       padding: '15px', 
+      color: '#000000', 
       borderRadius: '8px'
     }} 
     sx={{ display: state.openError }}
